e21724657af362c028a2fddc1e27fcea23f3fc97
AH400796 <a.hokhman@gmail.com>
2023-07-06 18:44:48 +0300

    12

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,26 @@
 import { defineConfig } from "vite";
+import { createHtmlPlugin } from "vite-plugin-html";
+import legacy from "@vitejs/plugin-legacy";
 
 export default defineConfig({
   base: "/portfolio/",
+  plugins: [
+    createHtmlPlugin({
+      inject: {
+        include: "./partials/*.html",
+      },
+      minify: false,
+      resolveOptions: {
+        conditions: ["include", "html"],
+      },
+    }),
+    legacy({
+      targets: ["defaults", "not IE 11"],
+    }),
+  ],
+  server: {
+    fs: {
+      strict: false,
+    },
+  },
 });
